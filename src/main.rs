71a967935fcb90ff9da05d7d618a5cf105745f56
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-08-24 12:22:55 +0900

    feat: データベースへ接続

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use futures::executor::block_on;
+use sea_orm::{Database, DbErr};
+
+const DATABASE_URL: &str = "postgres://postgres:postgres@127.0.0.1/bakery_backend";
+async fn run() -> Result<(), DbErr> {
+    let _db = Database::connect(DATABASE_URL).await?;
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() {
+    if let Err(err) = block_on(run()) {
+        panic!("{}", err);
+    }
 }
