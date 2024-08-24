24edfa528e30ab882e9c01f7ff8d2628c5d72832
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-08-24 14:07:22 +0900

    feat: BakeryデータをInsert

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,21 @@
 use futures::executor::block_on;
-use sea_orm::{Database, DbErr};
+use sea_orm::{ActiveValue, Database, DbErr, EntityTrait};
+
+mod entities;
+
+use entities::{prelude::*, *};
 
 const DATABASE_URL: &str = "postgres://postgres:postgres@127.0.0.1/bakery_backend";
 async fn run() -> Result<(), DbErr> {
-    let _db = Database::connect(DATABASE_URL).await?;
+    let db = Database::connect(DATABASE_URL).await?;
+
+    let happy_bakery = bakery::ActiveModel {
+        name: ActiveValue::Set("Happy Bakery".to_owned()),
+        profit_margin: ActiveValue::Set(0.0),
+        ..Default::default()
+    };
+
+    let _res = Bakery::insert(happy_bakery).exec(&db).await;
 
     Ok(())
 }
