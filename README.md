d0ab1af7e2dfebf0101e06bf19438b746b01c979
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-08-24 13:01:07 +0900

    feat: Create Chef Table

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
-### Run miration
+# データベースの更新
 
-```shell
+1.  マイグレーションファイルの作成
+2.  マイグレーションの実行
 
-$ DATABASE_URL="postgres://postgres:postgres@127.0.0.1/bakery_backend" sea-orm-cli migrate refresh
+  ```shell
 
-```
+  $ DATABASE_URL="postgres://postgres:postgres@127.0.0.1/bakery_backend" sea-orm-cli migrate refresh
+
+  ```
---
+++ migration/src/m20220101_000002_create_chef_table.rs
@@ -0,0 +1,50 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+use super::m20220101_000001_create_bakery_table::Bakery;
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20220101_000002_create_chef_table" // Make sure this matches with the file name
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Chef::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Chef::Id))
+                    .col(string(Chef::Name))
+                    .col(json(Chef::ContactDetails))
+                    .col(integer(Chef::BakeryId))
+                    .foreign_key(
+                        ForeignKey::create()
+                            .name("fk-chef-bakery_id")
+                            .from(Chef::Table, Chef::BakeryId)
+                            .to(Bakery::Table, Bakery::Id),
+                    )
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Chef::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Chef {
+    Table,
+    Id,
+    BakeryId,
+    Name,
+    ContactDetails,
+}
