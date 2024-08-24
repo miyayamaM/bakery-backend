347c40e0cc3719049f8cf41468b9f11a00928326
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-08-24 12:43:29 +0900

    feat: 最初のマイグレーションを実行

---
+++ README.md
@@ -0,0 +1,7 @@
+### Run miration
+
+```shell
+
+$ DATABASE_URL="postgres://postgres:postgres@127.0.0.1/bakery_backend" sea-orm-cli migrate refresh
+
+```
---
+++ migration/src/m20220101_000001_create_bakery_table.rs
@@ -0,0 +1,40 @@
+use sea_orm_migration::{prelude::*, schema::*};
+
+pub struct Migration;
+
+impl MigrationName for Migration {
+    fn name(&self) -> &str {
+        "m20220101_000001_create_bakery_table" // Make sure this matches with the file name
+    }
+}
+
+#[async_trait::async_trait]
+impl MigrationTrait for Migration {
+    async fn up(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .create_table(
+                Table::create()
+                    .table(Bakery::Table)
+                    .if_not_exists()
+                    .col(pk_auto(Bakery::Id))
+                    .col(string(Bakery::Name))
+                    .col(double(Bakery::ProfitMargin))
+                    .to_owned(),
+            )
+            .await
+    }
+
+    async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
+        manager
+            .drop_table(Table::drop().table(Bakery::Table).to_owned())
+            .await
+    }
+}
+
+#[derive(DeriveIden)]
+enum Bakery {
+    Table,
+    Id,
+    Name,
+    ProfitMargin,
+}
