826658dbc7171f677ae771605267319e0de8ec31
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-08-31 19:06:25 +0900

    feat: ChefをInsert

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use sea_orm::{ActiveValue, Database, DbErr, EntityTrait};
 mod entities;
 
 use entities::{prelude::*, *};
+use sea_orm_active_enums::EmploymentStatus;
 
 const DATABASE_URL: &str = "postgres://postgres:postgres@127.0.0.1/bakery_backend";
 async fn run() -> Result<(), DbErr> {
@@ -16,6 +17,16 @@ async fn run() -> Result<(), DbErr> {
     };
 
     let res = Bakery::insert(happy_bakery).exec(&db).await.unwrap();
+
+    let chef = chef::ActiveModel {
+        name: ActiveValue::Set("Manu".into()),
+        contact_details: ActiveValue::Set(Some("{}".into())),
+        bakery_id: ActiveValue::Set(res.last_insert_id),
+        employment_status: ActiveValue::Set(Some(EmploymentStatus::FullTime)),
+        ..Default::default()
+    };
+
+    Chef::insert(chef).exec(&db).await.unwrap();
 
     for bread_name in ["フランスパン", "あんぱん", "クイニーアマン"] {
         let bread = bread::ActiveModel {
