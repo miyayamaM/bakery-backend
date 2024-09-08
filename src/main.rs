77cce8092e48f3ac0e3ad8be6f6d62c9c2a02def
miyayamaM <pendulum.ngt.4010@gmail.com>
2024-09-08 19:40:40 +0900

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use factories::bakery_factory::BakeryFactory;
 use futures::executor::block_on;
-use sea_orm::{ColumnTrait, Database, DbErr, EntityTrait, QueryFilter};
+use sea_orm::{ColumnTrait, Database, DatabaseConnection, DbErr, EntityTrait, QueryFilter};
 mod entities;
 mod factories;
 
@@ -12,53 +12,8 @@ async fn run() -> Result<(), DbErr> {
 
     let _bakery_id = BakeryFactory::create(&db).await;
 
-    let bakeries: Vec<(bakery::Model, Vec<bread::Model>)> = Bakery::find()
-        .find_with_related(Bread)
-        .filter(bread::Column::Name.eq("メロンパン"))
-        .all(&db)
-        .await
-        .unwrap();
-
-    println!("メロンパンを売っているパン屋: {:#?}", &bakeries);
-
-    // let bakeries: Vec<(bakery::Model, Vec<bread::Model>)> = Bakery::find()
-    //     .find_with_related(Bread)
-    //     .filter(
-    //         Condition::all().add(
-    //             Expr::exists(
-    //                 Bread::find()
-    //                     .filter(bread::Column::Price.gt(1000))
-    //                     .into_query(),
-    //             )
-    //             .not(),
-    //         ),
-    //     )
-    //     .all(&db)
-    //     .await
-    //     .unwrap();
-
-    // サブクエリを作成: 高価なパン (価格 > 1000) を持つパン屋の ID を探す
-    let bakery_ids_selling_pricy_bread: Vec<i32> = Bread::find()
-        .find_with_related(Bakery)
-        .filter(bread::Column::Price.gt(1000))
-        .all(&db)
-        .await
-        .unwrap()
-        .into_iter()
-        .flat_map(|(_bread, bakeries)| bakeries.into_iter().map(|bakery| bakery.id))
-        .collect();
-
-    // メインクエリ: 高価なパンが存在しないパン屋を取得
-    let bakeries = Bakery::find()
-        .filter(bakery::Column::Id.is_not_in(bakery_ids_selling_pricy_bread))
-        .all(&db)
-        .await
-        .unwrap();
-
-    println!(
-        "すべての商品が1000円未満のbakery（商品がないパン屋も含む）: {:#?}",
-        &bakeries
-    );
+    get_bakeries_selling_melonpan(&db).await;
+    get_reasonable_bakeries(&db).await;
 
     Ok(())
 }
@@ -68,4 +23,40 @@ async fn main() {
     if let Err(err) = block_on(run()) {
         panic!("{}", err);
     }
+}
+
+async fn get_bakeries_selling_melonpan(db: &DatabaseConnection) {
+    let bakeries: Vec<(bakery::Model, Vec<bread::Model>)> = Bakery::find()
+        .find_with_related(Bread)
+        .filter(bread::Column::Name.eq("メロンパン"))
+        .all(db)
+        .await
+        .unwrap();
+
+    println!("メロンパンを売っているパン屋: {:#?}", &bakeries);
+}
+
+async fn get_reasonable_bakeries(db: &DatabaseConnection) {
+    // サブクエリを作成: 高価なパン (価格 > 1000) を持つパン屋の ID を探す
+    let bakery_ids_selling_pricy_bread: Vec<i32> = Bread::find()
+        .find_with_related(Bakery)
+        .filter(bread::Column::Price.gt(1000))
+        .all(db)
+        .await
+        .unwrap()
+        .into_iter()
+        .flat_map(|(_bread, bakeries)| bakeries.into_iter().map(|bakery| bakery.id))
+        .collect();
+
+    // メインクエリ: 高価なパンが存在しないパン屋を取得
+    let bakeries = Bakery::find()
+        .filter(bakery::Column::Id.is_not_in(bakery_ids_selling_pricy_bread))
+        .all(db)
+        .await
+        .unwrap();
+
+    println!(
+        "すべての商品が1000円未満のbakery（商品がないパン屋も含む）: {:#?}",
+        &bakeries
+    );
 }
